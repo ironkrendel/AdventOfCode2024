1af4a4e8760ad670deba0e37b60d2eb6ff3e79b3
ironkrendel <v.a.ryshkov@gmail.com>
2024-12-11 20:34:48 +0300

    Day11 solution

--- day11/day11_1.cpp
+++ day11/day11_1.cpp
@@ -1,19 +1,21 @@
 #include <stdio.h>
 #include <vector>
 #include <math.h>
+#include <unordered_map>
+#include <map>
 
 int NumDigitsCnt(unsigned long long num);
 
 int main() {
     FILE* input = fopen("input11.txt", "r");
 
-    std::vector<unsigned long long> data;
+    std::map<u_int64_t, u_int64_t> data_map;
 
     char c;
     unsigned long long num = 0;
     while ((c = fgetc(input)) != EOF) {
         if (c == ' ') {
-            data.push_back(num);
+            data_map[num]++;
             num = 0;
         }
         else {
@@ -21,38 +23,49 @@ int main() {
             num += c - '0';
         }
     }
-    data.push_back(num);
+    data_map[num]++;
 
     for (int lapse = 0;lapse < 75;lapse++) {
-        // for (std::size_t j = 0;j < data.size();j++) {
-        //     printf("%lld ", data[j]);
-        // }
-        // printf("\n");
-        for (std::size_t i = 0;i < data.size();i++) {
-            int digit_cnt;
-            if (data[i] == 0) {
-                data[i] = 1;
+        std::map<u_int64_t, u_int64_t> new_map = data_map;
+        for (std::map<u_int64_t, u_int64_t>::iterator it = data_map.begin();it != data_map.end();it++) {
+            if (it->second <= 0) continue;
+            int digit_cnt = NumDigitsCnt(it->first);
+            if (it->first == 0) {
+                new_map[0]-=it->second;
+                new_map[1]+=it->second;
             }
-            else if ((digit_cnt = NumDigitsCnt(data[i])) % 2 == 0) {
-                unsigned long long left_half = data[i] / pow(10, digit_cnt / 2);
-                unsigned long long right_half = data[i] % static_cast<long>(pow(10, digit_cnt / 2));
-                
-                data[i] = left_half;
-                data.insert(std::next(data.begin(), i + 1), right_half);
-                i++;
+            else if (digit_cnt % 2 == 0) {
+                u_int64_t left_half = it->first / pow(10, digit_cnt / 2);
+                u_int64_t right_half = it->first % static_cast<long>(pow(10, digit_cnt / 2));
+
+                new_map[it->first]-=it->second;
+                new_map[left_half]+=it->second;
+                new_map[right_half]+=it->second;
             }
             else {
-                data[i] *= 2024;
+                u_int64_t new_num = it->first * 2024;
+                new_map[it->first]-=it->second;
+                new_map[new_num]+=it->second;
             }
         }
-        printf("%d %ld\n", lapse, data.size());
-    }
-    for (std::size_t j = 0;j < data.size();j++) {
-        printf("%lld ", data[j]);
-    }
-    printf("\n");
+        data_map = new_map;
 
-    printf("%ld\n", data.size());
+        u_int64_t tmp_res = 0;
+        for (std::map<u_int64_t, u_int64_t>::iterator itt = data_map.begin();itt != data_map.end();itt++) {
+            if (itt->second <= 0) continue;
+            tmp_res += itt->second;
+        }
+        printf("%d - %ld\n", lapse, tmp_res);
+    }
+
+    u_int64_t result = 0;
+    for (std::map<u_int64_t, u_int64_t>::iterator it = data_map.begin();it != data_map.end();it++) {
+        if (it->second > 0) {
+            result += it->second;
+        }
+    }
+
+    printf("%ld\n", result);
 
     return 0;
 }
